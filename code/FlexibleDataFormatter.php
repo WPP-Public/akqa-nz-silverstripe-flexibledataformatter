132b8df38460429d4e0e9ef491c3641a160eaad1
Cam Spiers <camspiers@gmail.com>
2013-08-01 14:31:24 +1200

    UPDATE: Allow better support for objects you don't control

--- code/FlexibleDataFormatter.php
+++ code/FlexibleDataFormatter.php
@@ -6,17 +6,21 @@
 abstract class FlexibleDataFormatter
 {
     /**
-     * @param FlexibleDataFormatterInterface $obj
-     * @param array                          $config
+     * @param DataObject $obj
+     * @param array      $config
      * @return array|bool
      */
     public function convertDataObjectToArray(
-        FlexibleDataFormatterInterface $obj,
+        DataObject $obj,
         $config = array()
     ) {
         $content = array();
 
-        foreach ($obj->getAllowedFields($config) as $fieldName) {
+        $allowedFields = $obj instanceof FlexibleDataFormatterInterface
+            ? $obj->getAllowedFields($config)
+            : array_keys($obj->db());
+
+        foreach ($allowedFields as $fieldName) {
             if ($obj->hasMethod($fieldName)) {
                 $fieldValue = $obj->$fieldName();
             } else {
@@ -43,41 +47,45 @@ abstract class FlexibleDataFormatter
             }
         }
 
-        foreach ($obj->getAllowedHasOneRelations($config) as $relName) {
-            if ($obj->{$relName . 'ID'}) {
-                $content[$relName] = $this->convertDataObjectToArray($obj->$relName(), $config);
-            }
-        }
+        if ($obj instanceof FlexibleDataFormatterInterface) {
 
-        foreach ($obj->getAllowedHasManyRelations($config) as $relName) {
-            $items = $obj->$relName();
-            if ($items instanceof SS_List && count($items) > 0) {
-                $content[$relName] = array();
-                foreach ($items as $item) {
-                    $content[$relName][] = $this->convertDataObjectToArray($item, $config);
+            foreach ($obj->getAllowedHasOneRelations($config) as $relName) {
+                if ($obj->{$relName . 'ID'}) {
+                    $content[$relName] = $this->convertDataObjectToArray($obj->$relName(), $config);
                 }
             }
-        }
 
-        foreach ($obj->getAllowedManyManyRelations($config) as $relName) {
-            $items = $obj->$relName();
-            if ($items instanceof SS_List && count($items) > 0) {
-                $content[$relName] = array();
-                foreach ($items as $item) {
-                    $content[$relName][] = $this->convertDataObjectToArray($item, $config);
+            foreach ($obj->getAllowedHasManyRelations($config) as $relName) {
+                $items = $obj->$relName();
+                if ($items instanceof SS_List && count($items) > 0) {
+                    $content[$relName] = array();
+                    foreach ($items as $item) {
+                        $content[$relName][] = $this->convertDataObjectToArray($item, $config);
+                    }
                 }
             }
+
+            foreach ($obj->getAllowedManyManyRelations($config) as $relName) {
+                $items = $obj->$relName();
+                if ($items instanceof SS_List && count($items) > 0) {
+                    $content[$relName] = array();
+                    foreach ($items as $item) {
+                        $content[$relName][] = $this->convertDataObjectToArray($item, $config);
+                    }
+                }
+            }
+
         }
 
         return $content;
     }
     /**
-     * @param FlexibleDataFormatterInterface $obj
-     * @param array                          $config
+     * @param DataObject $obj
+     * @param array      $config
      * @return bool
      */
     public function convertDataObject(
-        FlexibleDataFormatterInterface $obj,
+        DataObject $obj,
         $config = array()
     ) {
         $content = $this->convertDataObjectToArray($obj, $config);
